e29a05e0f536356b27cd72edeea2f728590a468a
Jose <sixgg4@gmail.com>
2019-08-04 21:12:52 -0400

    practice data types

---
+++ Basics/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hello World";
+            char grade = 'A';//single quotation characters
+            int age = 30; //whole numbers or negative numbers
+            //there are other types such as float(less precise),  double, and decimal(most precise)
+            double gpa = 3.2;  //if zero added then change to decimal 
+            bool isMale = true; //boolean
+
+            //Could represent line inside a constant 
+            Console.Write(true)
+            Console.ReadLine();
+        }
+    }
+}
