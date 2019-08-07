cf011c68378bc2e50987b793cc9efbf7ba545c80
Jose <sixgg4@gmail.com>
2019-08-07 17:42:15 -0400

    practice loops, arrays. create exponent fuction

---
+++ Basics/Loops/Loops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhileLoopSample();
+            ForLoopSample();
+            
+            Console.ReadLine();
+        }
+    
+    static void ForLoopSample()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Value of i: {0}", i);
+            }
+        }
+    static void DoWhileLoopSample()
+        {
+            int index = 2;
+            do
+            {
+                Console.WriteLine("Running do while");
+            } while (index <= 2);
+        }
+    static void WhileLoopSample() {
+            int index = 1;
+            while (index <= 10)
+            {
+                Console.WriteLine($"Current count = {index}");
+                index++;
+
+            }
+        }
+    }
+}
---
+++ Projects/ExponentApp/ExponentApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExponentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(GetPower(5, 5));
+            Console.ReadLine();
+        }
+        static int GetPower(int baseNum, int powNum)
+        {
+            int result = 1;
+            for(int i = 0; i< powNum; i++)
+            {
+                result = result * baseNum;
+            }
+            return result;
+        }
+    }
+}
