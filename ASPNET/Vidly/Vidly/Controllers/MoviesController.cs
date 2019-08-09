959cd1105e8f11b11b62a55365daffe10fef31e3
Jose <sixgg4@gmail.com>
2019-08-08 20:31:21 -0400

    setup project

---
+++ ASPNET/Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random() //Default or "Index"
+        {
+            // creating an instance of the movie model 
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(); // The "View" is tied by name --> the Random method looks for the Random view
+        }
+    }
+}
---
+++ ASPNET/Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        //A Plain Old CLR Objects(POCO) does not have behavior or logic
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
