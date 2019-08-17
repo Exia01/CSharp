5614f8c063299ddf89f86e832d7909f2506bb3fc
Jose <sixgg4@gmail.com>
2019-08-16 20:22:23 -0400

    add annotation to model

--- ASPNET/Vidly/Vidly/Models/Customer.cs
+++ ASPNET/Vidly/Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // data annotations to override methods
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        //string type is nullable and there is not limit in characters numbers
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; } 
         public bool IsSubscribedToNewsletter { get; set; }
         // navigation property --> allows navigation from customer to membership type
         public MembershipType MembershipType { get; set; }
