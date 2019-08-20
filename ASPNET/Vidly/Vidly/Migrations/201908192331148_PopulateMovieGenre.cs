b79a0ac85ccebe1df018172cfeec0ccf724159b5
Jose <sixgg4@gmail.com>
2019-08-19 20:08:52 -0400

    update models

---
+++ ASPNET/Vidly/Vidly/Migrations/201908192331148_PopulateMovieGenre.cs
@@ -0,0 +1,26 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMovieGenre : DbMigration
+    {
+        public override void Up()
+        {
+            var random = new Random();
+            var genreList = new List<string> { "Suspense", "Novela", "Drama", "New-Age" };
+
+            int index = random.Next(genreList.Count +1); //somehow this didn't work lol
+            Sql($"UPDATE Movies SET Genre =  '{genreList[index]}' WHERE id = 1");
+            Sql($"UPDATE Movies SET Genre =  '{genreList[index]}' WHERE id = 2");
+            Sql($"UPDATE Movies SET Genre =  '{genreList[index]}' WHERE id = 3");
+            Sql($"UPDATE Movies SET Genre =  '{genreList[index]}' WHERE id = 4");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
