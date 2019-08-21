b71d798596fbc9f6ed16fcbada9c89ee52d88292
Jose <sixgg4@gmail.com>
2019-08-21 18:40:43 -0400

    Update Customers Views, Create CustomerFormViewModel

---
+++ ASPNET/Vidly/Vidly/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    //This ViewModel encapsulates all the models required by a view
+    public class CustomerFormViewModel
+    {   // since no functionality is needed from list IEnumerable enables looping
+        public IEnumerable<MembershipType> MemberhipTypes { get; set; }
+        public Customer Customer { get; set; }
+        public MembershipType MembershipType { get; set; }
+    }
+}
