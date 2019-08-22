8b8f5fadc30db3646a7caf70a6a778c11ca86a94
Jose <sixgg4@gmail.com>
2019-08-22 16:31:43 -0400

    update CustomerForm and CustomerController

--- ASPNET/Vidly/Vidly/Controllers/CustomersController.cs
+++ ASPNET/Vidly/Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -9,7 +10,7 @@ namespace Vidly.Controllers
     {
         // db context declaration
         private ApplicationDbContext _context;
-        
+
         //initializing in constructor
         public CustomersController()
         {
@@ -21,10 +22,6 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        public ActionResult New()
-        {
-            return View();
-        }
         // GET: Customers
         public ActionResult Index()
         {
@@ -37,13 +34,52 @@ namespace Vidly.Controllers
         }
 
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.membershipTypes.ToList();
+            ViewBag.Action = "Create";
+
+            //below we initialize a new view model which enables us to combine the models
+            var viewModel = new CustomerFormViewModel
+            {
+                MemberhipTypes = membershipTypes,
+            };
+            return View("CustomerForm",viewModel); //Overriding the "New" view
+        }
+
+        [HttpPost] //annotation --> can only be called only using POST method
+        public ActionResult Save(CustomerFormViewModel viewModel) //model binding --> mvc framework will automatically bind the model to the req data
+        {
+            if(viewModel.Customer.Id == 0 || viewModel.Customer.Id == null)
+            {
+
+            _context.customers.Add(viewModel.Customer); //can also just pass customer as the parameter
+            //line above is still just in memory -> we save with changes
+            _context.SaveChanges();
+                // action/controller
+            }
+            else
+            {
+                var cstmr = viewModel.Customer;
+                var customerInDb = _context.customers.Single(c => c.Id == cstmr.Id);
+                //Two ways of doing it
+                //TryUpdateModel(customerInDb, "", new string[]{"Name","Email"});
+                //can also use auto mapper, convention name tool
+                customerInDb.Name = cstmr.Name;
+                customerInDb.Birthday = cstmr.Birthday;
+                customerInDb.MembershipTypeId = cstmr.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = cstmr.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
         [Route("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
             ViewBag.Message = "Customer Detail";
-            var customer = _context.customers;
             //because of the SingleOrDefault extension method, this will query the db 
-            var foundCustomer = customer.SingleOrDefault(c => c.Id == id);
+            var foundCustomer = _context.customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             if (foundCustomer != null)
             {
                 return View(foundCustomer);
@@ -55,6 +91,40 @@ namespace Vidly.Controllers
             }
 
         }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Action = "Customer";
+            var customer = _context.customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                var viewmodel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MemberhipTypes = _context.membershipTypes.ToList(),
+
+                };
+                //overriding view so that we are using the "CustomerForm" Template and creating a view model to combine properties
+                return View("CustomerForm", viewmodel);
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
         // Using as mock DB
         //    private IEnumerable<Customer> GetCustomers()
         //    {
