7a83c7cb268b70fef5dd85be025f76cf65347512
Jose <sixgg4@gmail.com>
2019-08-24 20:28:39 -0400

    Update Customer.cs

--- ASPNET/Vidly/Vidly/Models/Customer.cs
+++ ASPNET/Vidly/Vidly/Models/Customer.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations; // data annotations to override methods
 
 namespace Vidly.Models
 {
-    public class Customer
-    {
+    public class Customer {
+    
         public int Id { get; set; }
         //string type is nullable and there is not limit in characters numbers
-        [Required]
+        [Required(ErrorMessage ="Please enter a name")]
         [StringLength(255)]
+    
         public string Name { get; set; }
 
         ///[Display(Name = "Date of Birth")]  // //Data annotation -> have to recompile to show difference
-        //[DataType(DataType.Date)]
+        
+        [Min18ifAMember]
         public DateTime? Birthday { get; set; } //question marks deems it as optional
 
         public bool IsSubscribedToNewsletter { get; set; }
         // navigation property --> allows navigation from customer to membership type
+        //[DefaultValue(null)]
         public MembershipType MembershipType { get; set; }
+
 
         // we can look up as foreign key
         [Display(Name ="Membership Type")]
-        public byte MembershipTypeId
+        //no validation
+        [Required(ErrorMessage = "Please select a membership")]
+        public byte MembershipTypeId //because it is byte and empty field passes a string throws error
         {
             get; set;
         }
